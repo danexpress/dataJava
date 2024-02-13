7390c0e72ef24de9e56fc54aae1e11b8cdd2c6a6
Fred Offei <112915741+danexpress@users.noreply.github.com>
2024-02-13 14:21:46 -0500

    methods sum

---
+++ mooc/Numberuno.java
@@ -0,0 +1,19 @@
+package mooc;
+
+public class Numberuno {
+    public static void main(String[] args) {
+        numberUno();
+        word();
+
+    }
+
+    public static int numberUno() {
+        return 1;
+
+    }
+
+    public static String word() {
+        return "three";
+
+    }
+}
